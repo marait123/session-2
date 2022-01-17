3f9f3a23aab92647352539ab0f67e3efe6ce0ca8
marait123 <max.ibrahim43@hotmail.com>
2022-01-17 18:08:40 +0200

    added cors and pagination

--- flaskr/__init__.py
+++ flaskr/__init__.py
@@ -4,56 +4,62 @@ from flask import Flask, json, request, jsonify, abort
 
 from models import setup_db, Greeting
 from flask_cors import CORS
+Page_count=2
+def create_app(test_config=None):
+    app = Flask(__name__)
+    setup_db(app)
 
-app = Flask(__name__)
-setup_db(app)
+    # cors = CORS(app, resources={r"/api/*": {"origins": "*"}})
+    cors = CORS(app, resources={"*": {"origins": "*"}})
 
-# cors = CORS(app, resources={r"/api/*": {"origins": "*"}})
-cors = CORS(app, resources={"*": {"origins": "*"}})
-
-greetings = {
-    'en': 'hello',
-    'es': 'Hola',
-    'ar': 'مرحبا',
-    'ru': 'Привет',
-    'fi': 'Hei',
-    'he': 'שלום',
-    'ja': 'こんにちは'
-}
-
-
-@app.route('/', methods=['GET'])
-def index():
-    return "<h1>hello friends</h1>"
+    # example greetings
+    greetings = {
+        'en': 'hello',
+        'es': 'Hola',
+        'ar': 'مرحبا',
+        'ru': 'Привет',
+        'fi': 'Hei',
+        'he': 'שלום',
+        'ja': 'こんにちは'
+    }
 
 
-@app.route('/greetings', methods=['GET'])
-def greeting_all():
-    # TODO implement pagination
-    greetings = Greeting.query.all()
-    greetings = [greeting.format() for greeting in greetings]
-    return jsonify({'greetings': greetings})
+    @app.route('/', methods=['GET'])
+    def index():
+        return "<h1>hello friends</h1>"
 
 
-@app.route('/greetings/<lang>', methods=['GET'])
-def greeting_one(lang):
-    greeting = Greeting.query.filter_by(lang == lang)
-    print(lang)
-    # if(lang not in greetings):
-    if(not greeting):
-        return jsonify({'error': "no greeting in this language"})
-    return jsonify({'greeting': greeting})
+    @app.route('/greetings', methods=['GET'])
+    def greeting_all():
+        page = request.args.get('page',1, type=int)
+        pagination = Greeting.query.paginate(page, per_page=Page_count,error_out=False)
+        greetings = pagination.items
+        # TODO implement pagination
+        # greetings = Greeting.query.all()
+        greetings = [greeting.format() for greeting in greetings]
+        return jsonify({'greetings': greetings, 'count':pagination.total})
 
 
-@app.route('/greetings', methods=['POST'])
-def greeting_add():
-    info = request.get_json()
-    if('lang' not in info or 'greeting' not in info):
-        abort(422)
-    # greetings[info['lang']] = info['greeting']
-    greeting = Greeting(info['lang'], info['greeting'])
-    greeting.insert()
-    return jsonify({'greeting': greeting.format()})
 
-if __name__ == "__main__":
-    app.run(debug=True)
+    @app.route('/greetings/<lang>', methods=['GET'])
+    def greeting_one(lang):
+        greeting = Greeting.query.filter_by(lang = lang).first()
+        print(lang)
+        # if(lang not in greetings):
+        if(not greeting):
+            return jsonify({'error': "no greeting in this language"})
+        return jsonify({'greeting': greeting.format()})
+
+
+    @app.route('/greetings', methods=['POST'])
+    def greeting_add():
+        info = request.get_json()
+        if('lang' not in info or 'greeting' not in info):
+            abort(422)
+        # greetings[info['lang']] = info['greeting']
+        greeting = Greeting(info['lang'], info['greeting'])
+        greeting.insert()
+        return jsonify({'greeting': greeting.format()})
+    return app
+    # if __name__ == "__main__":
+    #     app.run(debug=True)
--- test_app.py
+++ test_app.py
@@ -1,38 +1,87 @@
+from http.client import ImproperConnectionState
 import os
 import unittest
 import json
 from flask_sqlalchemy import SQLAlchemy
-
-from flaskr import app
-# TODO: 
-# make sure you create a database named hello_test in psql 
+from models import Greeting, setup_db, db
+from flaskr import create_app
+# TODO:
+# make sure you create a database named hello_test in psql
 database_name = "hello_test"
 database_path = 'postgresql://postgres:123456@localhost:5432/{}'.format(
     database_name)
+greetings = [{'lang': 'en', 'greeting': 'hello'},
+             {'lang': 'es', 'greeting': 'Hola'},
+             {'lang': 'ar', 'greeting': 'مرحبا'},
+             {'lang': 'ru', 'greeting': 'Привет'},
+             {'lang': 'fi', 'greeting': 'Hei'},
+             {'lang': 'he', 'greeting': 'שלום'},
+             {'lang': 'ja', 'greeting': 'こんにちは'}]
 
 
-class TriviaTestCase(unittest.TestCase):
+class GreetingTestCase(unittest.TestCase):
     """This class represents the trivia test case"""
 
     def setUp(self):
-        """Define test variables and initialize app."""
-        self.app = app
+        """Define test variables and initialize app.
+            it is run before each test
+        """
+        self.app = create_app()
         self.client = self.app.test_client
+        self.database_path = database_path
+        setup_db(self.app, self.database_path)
 
+        # binds the app to the current context
+        # each flask app has a context that includes all of the apps configuration like database, password, etc
+        with self.app.app_context():
+            # self.db = SQLAlchemy()
+            self.db = db
+            self.db.init_app(self.app)
+            # clean the database beforehand
+            self.db.drop_all()
+            self.db.session.commit()
+
+            # create all tables
+            self.db.create_all()
+            self.db.session.commit()
+
+        with self.app.app_context():
+            # insert the categories
+            for greeting in greetings:
+                cat = Greeting(**greeting)
+                self.db.session.add(cat)
+                self.db.session.commit()
+    # in case you want to clean the database after each request
     def tearDown(self):
-        """Executed after reach test"""
+        """Executed after each test"""
+        with self.app.app_context():
+            # clean the database beforehand
+            self.db.drop_all()
+            self.db.session.commit()
         pass
 
-    # testing the categories
-    def test_get_categories_ok(self):
-        print('hello')
-        res = self.client().get('/greeting')
-        print(res)
-        data = json.loads(res.data)
-        print(data)
+    # testing the greetings
+    def test_get_greetings_ok(self):
+        # print('hello')
+        print('hello test_get_greetings_ok')
+        
+        res = self.client().get('/greetings') # res is of a type stream
+        # print(res.data) # res.data is a of type string of characters
+        new_greeetings = json.loads(res.data) # data is a of type string of characters
+        # print(new_greeetings)
         self.assertEqual(200, 200)
 
-
+    #TODO: implement a test case to test getting one greeting
+    def test_one_greeting(self):
+        print('hello test_one_greeting')
+        res = self.client().get(f'/greetings/en')
+        # print("res " , res)
+        new_greeting = json.loads(res.data)       
+        # print("new_greeting ",new_greeting)
+         
+        self.assertEqual(200, 200)
+        self.assertEqual(new_greeting["greeting"]['greeting'], greetings[0]["greeting"])
+        
 # Make the tests conveniently executable
 if __name__ == "__main__":
     unittest.main()
